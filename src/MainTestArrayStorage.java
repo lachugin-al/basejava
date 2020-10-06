dc7985f035d82bd0a4074f7c0c004efff43a6c86
antonlachugin <lachugin.a.l@icloud.com>
2020-10-06 23:57:16 +0300

    HW3

--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -27,6 +27,8 @@ public class MainTestArrayStorage {
         printAll();
         ARRAY_STORAGE.delete(r1.getUuid());
         printAll();
+        ARRAY_STORAGE.update(r2);
+        printAll();
         ARRAY_STORAGE.clear();
         printAll();
 
